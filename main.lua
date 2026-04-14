4332d3b4f7142e44a1d133adbc8e8a3f50ef5cab
Ikzyw <wonmy27@gmail.com>
2026-04-14 14:34:19 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+local placeId = game.PlaceId
+
+if placeId = 114043013325512 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ikzyw/NavyHub/refs/heads/main/obfuscated_NH.MBG.lua"))()
+elseif placeId = 103854444055060 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ikzyw/NavyHub/refs/heads/main/obfuscated_Nvy.SA.lua.txt"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ikzyw/NavyHub/refs/heads/main/obfuscated_NH.Unv.lua"))()
+end
